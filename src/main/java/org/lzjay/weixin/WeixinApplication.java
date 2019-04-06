0ad5ea0c6625ad76d1c12091f7a13388e625de51
Jay Lin <Jay Lin@DESKTOP-7A6QDNG>
2019-04-07 00:12:41 +0800

    最新提交day4

--- src/main/java/org/lzjay/weixin/WeixinApplication.java
+++ src/main/java/org/lzjay/weixin/WeixinApplication.java
@@ -1,11 +1,37 @@
 package org.lzjay.weixin;
 
+import org.lzjay.weixin.domain.InMessage;
+import org.lzjay.weixin.service.JsonRedisSerializer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 
 @SpringBootApplication
 public class WeixinApplication {
 
+	@Bean
+	public XmlMapper xmlMapper() {
+		XmlMapper mapper = new XmlMapper();
+		return mapper;
+	}
+	
+	@Bean
+	public RedisTemplate<String, ? extends InMessage> inMessageTemplate(//
+			@Autowired RedisConnectionFactory connectionFactory) {
+
+		RedisTemplate<String, ? extends InMessage> template = new RedisTemplate<>();
+		template.setConnectionFactory(connectionFactory);
+		// 使用序列化程序完成对象的序列化和反序列化，可以自定义
+		  template.setValueSerializer(new JsonRedisSerializer<InMessage>());
+
+		return template;
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(WeixinApplication.class, args);
 	}
